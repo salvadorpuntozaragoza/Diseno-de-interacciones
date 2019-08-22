eaf2bad3d03b2660a6563712f792fd727ffe4365
Salvador Zaragoza Garcia <salvador.zaragoza@uabc.edu.mx>
2019-08-22 16:28:18 -0700

    By: Salvador Zaragoza
    *Fisica a la esfera y propiedad de rebote
    *Script que imprime mensaje al detectar colision con el piso

---
+++ DDI2019/Assets/CollisionDetected.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetected : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Debug.Log("Inicia juego");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision){
+      Debug.Log("Colision detectadaaaa con " + collision.gameObject.name);
+    }
+}
