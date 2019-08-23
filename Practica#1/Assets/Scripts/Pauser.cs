5ed125a4f95967d48246bec6b89c9ae1133c6821
Salvador Zaragoza Garcia <salvador.zaragoza@uabc.edu.mx>
2019-08-23 16:01:13 -0700

    Implemented functionality to pause Menu

--- Practica#1/Assets/Scripts/Pauser.cs
+++ Practica#1/Assets/Scripts/Pauser.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Pauser : MonoBehaviour {
-	private bool paused = false;
-	
+	[SerializeField] private bool muted = false;
+	[SerializeField] private bool paused = false;
+	[SerializeField] private GameObject pauseMenu;
+	[SerializeField] private AudioSource music;
+
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyUp(KeyCode.P))
@@ -11,9 +16,32 @@ public class Pauser : MonoBehaviour {
 			paused = !paused;
 		}
 
-		if(paused)
+		if(paused){
+			music.volume = 0.1F;
 			Time.timeScale = 0;
-		else
+			pauseMenu.SetActive(true);
+		}
+		else{
+			music.volume = 0.5F;
 			Time.timeScale = 1;
+			pauseMenu.SetActive(false);
+		}
+	}
+
+	public void Resume(){
+		paused = false;
+	}
+
+	public void Mute(){
+		muted = !muted;
+
+		if(muted)
+			music.mute = true;
+		else
+			music.mute = false;
+	}
+
+	public void Restart(){
+		SceneManager.LoadScene("Level");
 	}
 }
