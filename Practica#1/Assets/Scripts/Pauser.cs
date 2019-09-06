a000ef32fba60be6e8186498a32b9c19098b799c
Salvador zaragoza <Salvador.zaragoza@uabc.edu.mx>
2019-09-06 12:01:36 -0700

    *didn't do much just messing with the code

--- Practica#1/Assets/Scripts/Pauser.cs
+++ Practica#1/Assets/Scripts/Pauser.cs
@@ -2,16 +2,24 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class Pauser : MonoBehaviour {
-	[SerializeField] private bool muted = false;
-	[SerializeField] private bool paused = false;
-	[SerializeField] private GameObject pauseMenu;
-	[SerializeField] private GameObject HUD;
-	[SerializeField] private AudioSource music;
+    [SerializeField] private bool muted = false;
+    [SerializeField] private bool paused = false;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject HUD;
+    [SerializeField] private AudioSource music;
+    [SerializeField] private GameObject fps;
+    [SerializeField] private MouseLook mouse;
 
-	// Update is called once per frame
-	void Update () {
+    private void Start()
+    {
+        mouse.lockCursor = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
 		if(Input.GetKeyUp(KeyCode.P))
 		{
 			paused = !paused;
@@ -22,12 +30,14 @@ public class Pauser : MonoBehaviour {
 			Time.timeScale = 0;
 			pauseMenu.SetActive(true);
 			HUD.SetActive(false);
+            mouse.lockCursor = false;
 		}
 		else{
 			music.volume = 0.5F;
 			Time.timeScale = 1;
 			pauseMenu.SetActive(false);
 			HUD.SetActive(true);
+            mouse.lockCursor = true;
 		}
 	}
 
