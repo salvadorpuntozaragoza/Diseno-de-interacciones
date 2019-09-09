5d4a511a6da2496fc67133dec8a94d24617dc4c6
Salvador Zaragoza Garcia <salvador.zaragoza@uabc.edu.mx>
2019-09-08 17:56:56 -0700

    *added Script for cameraController

---
+++ Practica#1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform cameraTarget;
+    [Range(1, 10)]
+    public float rotationSpeed = 1f;
+    private float mouseX, mouseY;
+    public Vector2 limitY = new Vector2(-30, 30);
+
+    void Start()
+    {
+        Cusror.visible = false;
+        Cursor.LockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, limitY.x, limitY.y);
+        cameraTarget.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Transform.LookAt(cameraTarget);
+    }
+}
