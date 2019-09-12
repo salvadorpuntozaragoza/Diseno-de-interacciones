67c9d431b080f19bdbfc25b1f74de4b8d6be604e
Salvador Zaragoza Garcia <salvador.zaragoza@uabc.edu.mx>
2019-09-12 08:35:53 -0700

    *added collide detection script

---
+++ Practica#1/Assets/collideDetection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collideDetection : MonoBehaviour
+{
+
+  [SerializeField] private bool entered;
+    // Start is called before the first frame update
+    void Start()
+    {
+      entered = false;
+    }
+
+    void OnTriggerEntered(Collider c){
+      entered = true;
+    }
+
+    void OnTriggerExited(Collider c){
+      entered = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
