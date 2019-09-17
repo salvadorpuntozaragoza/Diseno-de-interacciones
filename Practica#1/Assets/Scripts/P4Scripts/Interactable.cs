0c9f8270bd30fab4a148526ec43c7af05a7552c9
Salvador zaragoza <Salvador.zaragoza@uabc.edu.mx>
2019-09-16 23:03:12 -0700

    *Added sprites for potions, imported code for practice 4, and created the inventory menu

---
+++ Practica#1/Assets/Scripts/P4Scripts/Interactable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour 
+{
+	bool isInsideZone;
+	public KeyCode interactionKey = KeyCode.I;
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		if(isInsideZone)
+		{
+			if(Input.GetKeyDown(interactionKey))
+			{
+				Interact();
+			}
+		}
+	}
+
+	/// <summary>
+	/// OnTriggerEnter is called when the Collider other enters the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerEnter(Collider other)
+	{
+		if(!other.CompareTag("Player"))
+			return;
+		// Debug.Log("Entraste en la esfera!");
+		// rend.material.color = newColor;
+		isInsideZone = true;
+	}
+
+	/// <summary>
+	/// OnTriggerExit is called when the Collider other has stopped touching the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerExit(Collider other)
+	{
+		isInsideZone = false;
+	}
+
+	public virtual void Interact()
+	{
+
+	}
+}
---
+++ Practica#1/Assets/Scripts/P4Scripts/InventoryUI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour 
+{
+	public GameObject inventoryPanel;
+	private Inventory inventory;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		inventory = FindObjectOfType<Inventory>();
+		if (inventory == null)
+		{
+			Debug.LogWarning("No se encontró el Inventario");
+			return;
+		} 
+		inventoryPanel.SetActive(false);
+		inventory.onItemChange += UpdateUI;
+	}
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+			Debug.Log("UI Off");
+			inventoryPanel.SetActive(!inventoryPanel.activeSelf);
+            UpdateUI();
+		}
+	}
+
+	void UpdateUI()
+	{
+        Debug.Log("UIUpdated");
+		Slot[] slots = GetComponentsInChildren<Slot>();
+		for (int i = 0; i < slots.Length; i++)
+		{
+			if(i < inventory.items.Count)
+			{
+				slots[i].AddItem(inventory.items[i]);
+                Debug.Log("Item added to slot" + i);
+			}
+			else
+			{
+				slots[i].ClearSlot();
+			}
+		}
+	}
+}
