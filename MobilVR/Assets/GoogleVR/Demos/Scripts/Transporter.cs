3b7f6378631e170777c5d2400d146e3b8fc051cc
Salvador zaragoza <Salvador.zaragoza@uabc.edu.mx>
2019-10-04 16:36:40 -0700

    *Added tile for player movement, whenever u tilt the camera down, the player will move forward to where the camera is facing

--- MobilVR/Assets/GoogleVR/Demos/Scripts/Transporter.cs
+++ MobilVR/Assets/GoogleVR/Demos/Scripts/Transporter.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class Transporter : MonoBehaviour
 {
+
+    [SerializeField] private float distance;
+    [SerializeField] private GameObject player;
+    private bool isGazed;
+    [SerializeField] private GameObject movementTile;
     // Start is called before the first frame update
     void Start()
     {
-        
+        distance = 2f;
+        isGazed = false; 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isGazed)
+        {
+            player.transform.position = player.transform.position + Camera.main.transform.forward *
+                distance * Time.deltaTime;
+            player.transform.position = new Vector3(player.transform.position.x, 1.9f, player.transform.position.z);
+        }
 
+        //movementTile.transform.position = Camera.main.transform.position;
+        movementTile.transform.position = new Vector3(Camera.main.transform.position.x, 1.2f, Camera.main.transform.position.z);
     }
 
     public void Entered()
     {
-        Debug.Log("Gazed at");
+        isGazed = true;
     }
 
     public void Exited()
     {
-        Debug.Log("Gazed off");
+        isGazed = false;
     }
 }
