71411d9de91baed12810b6fe081db83d52d9b010
Salvador Zaragoza <salvador.zaragoza@uabc.edu.mx>
2019-10-04 07:43:43 -0700

    *All project ready

---
+++ MobilVR/Assets/GoogleVR/Demos/Scripts/MovementScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+
+    [SerializeField] private int direction;
+    private bool gazedAt;
+    [SerializeField] private int speed;
+    [SerializeField] private GameObject trans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = -1;
+        gazedAt = false;
+        speed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gazedAt)
+        {
+            
+        }
+    }
+
+    public void setGazedAt(bool isGazed)
+    {
+        this.gazedAt = isGazed;
+    }
+}
